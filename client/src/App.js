c8b73900bba23e80a79320658aeeb56eb8e68ff2
Eric@TechLearn <ee.ren@eevolve.club>
2020-03-08 21:49:08 +0800

    fixed Router and docker-compose issues

--- client/src/App.js
+++ client/src/App.js
@@ -16,10 +16,10 @@ class App extends Component {
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
-        </div>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
         </div>
       </Router>
     );  
